de672660b60dc74b2fbb0a7a0a84ad9a20d029a2
awemulya <awemulya@gmail.com>
2016-05-30 12:50:27 +0545

    source path reader and sample source file

--- utils/source.py
+++ utils/source.py
@@ -1,1 +1,14 @@
-SOURCE_PATH = '/home/awemulya/Documents/chess/KingBaseLite2016-03-pgn'
+import os
+SOURCE_PATH = ''
+
+
+class SourcePath(object):
+
+    def get_source_path(self):
+        source_dir = os.path.join(os.getcwd(), 'source', 'source.txt')
+        with open(source_dir) as source:
+            SOURCE_PATH = source.read()
+        return SOURCE_PATH
+
+
+
